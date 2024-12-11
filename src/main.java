759f7cdefe3dc4d2c9e533e2a6fb4ea59ddf5bbd
Merry <rozzimario18@gmail.com>
2024-12-11 18:24:57 +0100

    test for the three-way-brench

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main (String[] args) {
-        String message = "branch exercise";
+        String message = "test della three way branching";
         System.out.println(message);
     }
 }
